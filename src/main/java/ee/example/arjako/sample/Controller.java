18e7a0cc3ae2eadf5c63f94ba28b0b2ada6e0142
Artjom Jakovlev <arjako@ttu.ee>
2019-02-08 23:00:51 +0200

    Now serves webpage at localhost:8080

--- src/main/java/ee/example/arjako/sample/Controller.java
+++ src/main/java/ee/example/arjako/sample/Controller.java
@@ -2,13 +2,15 @@ package ee.example.arjako.sample;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin
 @RestController
+@CrossOrigin
+@RequestMapping("/api")
 public class Controller {
 
-    @GetMapping(value = "/", produces = "text/plain")
+    @GetMapping(value = "/hello", produces = "text/plain")
     public String index() {
         return "Greetings from Spring Boot!";
     }
